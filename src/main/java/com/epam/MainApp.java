b78f44a4454baf1b2547126c72321c8baf877d29
aldo <aldos2002@gmail.com>
2018-05-08 15:09:55 +0600

    Added assembly of exec jar.

--- src/main/java/com/epam/MainApp.java
+++ src/main/java/com/epam/MainApp.java
@@ -1,9 +1,11 @@
+package com.epam;
+
 /**
  * Created by admin
  * on 08.05.2018.
  */
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Hello dockerized world!");
+        System.out.println("Hello dockerized/maven world!");
     }
 }
