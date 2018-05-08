ecf0d95f31a04789cba8f9ffc8fc207f661e63f6
aldo <aldos2002@gmail.com>
2018-05-08 13:48:57 +0600

    Initial code commit.

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,9 @@
+/**
+ * Created by admin
+ * on 08.05.2018.
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello dockerized world!");
+    }
+}
